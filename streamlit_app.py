1b773333a826b260e8f0b66d7512f919d35c49a4
sklapman <sklapman@users.noreply.github.com>
2023-10-05 13:35:01 -0400

    Update streamlit_app.py
    added selector

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,9 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text("🥑🍞 Avocado Toast")
 
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.dataframe(my_fruit_list)
   
