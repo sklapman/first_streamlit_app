4354847a3edb1875c447e74d330d402bbbcc81e6
sklapman <sklapman@users.noreply.github.com>
2023-10-05 11:40:21 -0400

    Update streamlit_app.py
    fixed bugs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
-stremlit.header("Breakfast Menu")
+streamlit.header("Breakfast Menu")
 streamlit.text("Kale, Spinach and Rocket Smoothie")
