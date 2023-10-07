eae4693c50fc6f3dbc1654706f0627138bccdc9d
sklapman <sklapman@users.noreply.github.com>
2023-10-07 07:34:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,4 +33,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # output to screen as table
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.header('Test Update') 
   
